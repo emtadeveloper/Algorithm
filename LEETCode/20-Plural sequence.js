cebb5ff49d4aa37115f92dab834588aa5ddaee83
emtadeveloper <emta137671@gmail.com>
2023-02-04 16:01:41 -0800

    20

---
+++ LEETCode/20-Plural sequence.js
@@ -0,0 +1,42 @@
+// Emad Taghipour 
+
+// یک آرایه داریم که میخایم بدونیم از چن تا از اعضای آن با ظرط توالی پشت سر هم میان و
+// میتونن پشت سر هم قرار بگیرند به شرطی که طولشون کمتر از مقدار مشخص شده باشد
+
+var source1 = [1, 10, 2, 3, 14];
+var x1 = 10;
+
+var Source2 = [2, 5, 9];
+var x2 = 10;
+
+var Source3 = [2, 3, 1, 5, 4];
+var x3 = 10;
+
+var Source3 = [2, 3, 1];
+var x3 = 10;
+
+const checkConvertArray = (arr, target) => {
+  var targetArray = [];
+  for (index = 0; index < arr.length; index++) {
+    if (arr[index] <= target) {
+      let sum = 0;
+      let newArray = [];
+      for (i = index; i < arr.length; i++) {
+        if (sum + arr[i] <= 10) {
+          sum = sum + arr[i];
+          newArray.push(arr[i]);
+          targetArray.push([...newArray]);
+        } else {
+          break;
+        }
+      }
+      sum = 0;
+      newArray = [];
+    }
+  }
+  return [...targetArray];
+};
+
+console.log(checkConvertArray(source1, x1));
+console.log(checkConvertArray(Source2, x2));
+console.log(checkConvertArray(Source3, x3));
